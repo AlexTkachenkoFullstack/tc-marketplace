8a19366c97c067aef945f72788db1f0601ceadde
Eleonora Demikh <deademikh@gmail.com>
2023-10-13 17:21:36 +0300

    login v1

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -1,20 +1,24 @@
 import { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
 
 import styles from './Layout.module.scss';
 
 export const Layout: FC = () => {
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
     <>
+      {path !== '/login' && <Header />}
       <Header />
 
       <main className={styles.main}>
           <Outlet />
       </main>
 
-      <Footer />
+      {path !== '/login' && <Footer />}
     </>
   );
 };
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,8 +3,18 @@ import styles from './Login.module.scss';
 
 export const LoginPage: FC = () => {
   return (
-    <div className={`${styles.Container}`}>
-      some text
+    <div className={styles.Container}>
+      <section className={styles.Login_container}>
+        <span className={styles.Login_step}>Крок 1/3</span>
+        <h2 className={styles.Login_title}>Реєстрація</h2>
+        <span className={styles.Login_login}>
+          Вже є акаунт?<a href="/#">Увійти</a>
+        </span>
+        <input type='email' placeholder='E-mail' className={styles.Login_email}></input>
+        <button className={styles.Login_btn}>Увійти</button>
+        <img src='../../assets/images/horizontal-line.png' alt=''/>
+        <button className={styles.Login_googleBtn}>Увійти через Google</button>
+      </section>
     </div>
   );
 };
