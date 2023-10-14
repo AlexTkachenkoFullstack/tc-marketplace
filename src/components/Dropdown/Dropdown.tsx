ef8df0605ea4fcda6fe2e81f90a17545a9829f9b
BIVXXVIII <108072004+BIVXXVIII@users.noreply.github.com>
2023-10-14 18:44:56 +0300

    inputFocusChange

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,5 @@
 import {
-  FC, useState, useRef, useCallback, useEffect,
+    FC, useState, useRef, useCallback, useEffect
 } from 'react';
 import styles from './Dropdown.module.scss';
 
@@ -9,100 +9,135 @@ import close from '../../assets/icons/close.svg';
 import useClickOutside from 'helpers/hooks/useClickOutside';
 
 type Props = {
-  label: string;
-  startValue: string;
-  options: string[];
+    label: string;
+    startValue: string;
+    options: string[];
 };
 
+// to do:
+// work with input focus
 export const Dropdown: FC<Props> = (props) => {
-  const {
-    label,
-    startValue,
-    options,
-  } = props;
+    const {
+        label,
+        startValue,
+        options,
+    } = props;
 
-  const [isActive, setIsActive] = useState(false);
-  const [option, setOption] = useState(startValue);
+    const [isActive, setIsActive] = useState(false);
+    const [option, setOption] = useState(startValue);
+    const [filterValue, setfilterValue] = useState('')
 
-  const dropdownRef = useRef<HTMLDivElement>(null);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null)
+    const changeOption = (newOption: string) => {
+        setOption(newOption);
+        setIsActive(false);
+    };
 
-  const changeOption = (newOption: string) => {
-    setOption(newOption);
-    setIsActive(false);
-  };
-  //   const params: SearchParams = {
-  //     [searchParamName]: paramValue,
-  //   };
 
-  //   const additionalParams = Object.entries(additionalSearhParams);
+    const filterValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setfilterValue(e.target.value)
+    }
 
-  //   if (additionalParams.length) {
-  //     additionalParams.forEach(([option, value]) => {
-  //       params[option] = value;
-  //     });
-  //   }
+    useClickOutside(dropdownRef, useCallback(() => {
+        setIsActive(false);
+    }, []));
 
-  //   return params;
-  // };
+    useEffect(() => {
+        setOption(startValue);
+    }, [startValue]);
 
-  useClickOutside(dropdownRef, useCallback(() => {
-    setIsActive(false);
-  }, []));
 
-  useEffect(() => {
-    setOption(startValue);
-  }, [startValue]);
+    const renderOptions = (optionsArr: string[]) => {
 
-  return (
-    <div
-      className={styles.container}
-      ref={dropdownRef}
-    >
-      {(option !== startValue) && (
-        <span className={styles.label}>{label}</span>
-      )}
-
-      <button
-        className={styles.trigger}
-        type="button"
-        onClick={() => setIsActive((prevState) => !prevState)}
-      >
-        <div className={`${styles.trigger_content} ${isActive ? styles.trigger_content_active : ''}`}>
-        <span className={styles.text}>{option}</span>
-
-        <div className={styles.icons}>
-          {!isActive ? (
-            <img
-              src={arrowDown}
-              alt="down"
-              className={styles.icon}
-            />
-          ) : (
-            <img
-              src={close}
-              alt="close"
-              className={styles.icon}
-            />
-          )}
-        </div>
-        </div>
-      </button>
-
-      {isActive && (
-        <ul className={styles.list}>
-          {options.map(currentOption => (
-            <li className={styles.listItem} key={currentOption}>
-              <span
-                className={styles.listLink}
-                onClick={() => changeOption(currentOption)}
-              >
-                {currentOption}
-              </span>
+        if (optionsArr.length === 0) return (
+            <li className={styles.listItem}>
+                <span className={`${styles.listLink} ${styles.listLink_inactive}`}>no matches found</span>
             </li>
-          ))}
-        </ul>
-      )}
+        )
 
-    </div>
-  );
+        return optionsArr.map(currentOption => (
+            <li className={styles.listItem} key={currentOption}>
+                <span
+                    className={styles.listLink}
+                    onClick={() => {
+                        changeOption(currentOption)
+                    }}
+                >
+                    {currentOption}
+                </span>
+            </li>
+        ))
+    }
+
+    const filterOptions = (option: string) => {
+
+        if (filterValue.length === 0) return true
+        return option.toLowerCase().includes(filterValue.toLowerCase().trim())
+
+    }
+
+    useEffect(() => { setfilterValue('') }, [])
+
+    return (
+        <div
+            className={styles.container}
+            ref={dropdownRef}
+        >
+            {((isActive && filterValue.trim().length === 0) || option !== startValue) && (
+                <span className={styles.label}>{label}</span>
+            )}
+
+            <button
+                className={styles.trigger}
+                type="button"
+                onClick={(event) => {
+                    if (event.target === inputRef.current) return
+                    setIsActive((prevState) => !prevState)
+                }
+                }
+            >
+                <div className={`${styles.trigger_content} ${isActive ? styles.trigger_content_active : ''}`}
+                >
+                    {isActive ? <input
+                        onKeyUp={(e) => {
+                            if (e.key === ' ') { e.preventDefault() }
+                        }}
+                        className={styles.searchInput}
+                        type="text"
+                        onChange={filterValueHandler}
+                        placeholder={label}
+                        value={filterValue}
+                        ref={inputRef}
+                    /> :
+                        <div className={styles.text}>
+                            {option}
+                        </div>}
+
+                    <div className={styles.icons}>
+                        {!isActive ? (
+                            <img
+                                src={arrowDown}
+                                alt="down"
+                                className={styles.icon}
+                            />
+                        ) : (
+                            <img
+                                src={close}
+                                alt="close"
+                                className={styles.icon}
+                            />
+                        )}
+                    </div>
+                </div>
+            </button>
+
+            {isActive && (
+                <ul className={styles.list}>
+                    {renderOptions(options.filter(filterOptions))}
+                </ul>
+            )}
+
+        </div>
+    );
 };
