520ef7e334ef6efb91039ffbf419d319b7959e25
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-10-22 20:13:53 +0100

    add middleware in redux for auth

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,11 +8,13 @@ import { useAppDispatch, useAppSelector } from 'redux/hooks';
 import { fetchNewCars, fetchPopularCars, fetchViewedCars } from 'redux/cars/operations';
 import { getNewCars, getPopularCars, getViewedCars } from 'redux/cars/selectors';
 import { ICar } from 'types/IСar';
+import { isAuthUser } from 'redux/auth/selectors';
 const POPULARLIMIT=9;
 export const HomePage: React.FC = () => { 
   const newCars=useAppSelector(getNewCars);
   const popularCars=useAppSelector(getPopularCars);
   const viewedCars=useAppSelector(getViewedCars);
+  const isAuth=useAppSelector(isAuthUser)
   const [popularCarsToShow, setPopularCarsToShow]=useState<ICar[]>([])
   const [currentPage, setCurrentPage]=useState<number>(0);
   const [showButtonLoadMore, setShowButtonLoadMore]=useState<boolean>(true)
@@ -25,7 +27,7 @@ export const HomePage: React.FC = () => {
       setCurrentPage(0)
       dispatch(fetchViewedCars())
       dispatch(fetchNewCars())
-      dispatch(fetchPopularCars())
+      dispatch(fetchPopularCars())  
   }, [dispatch])
 
 
@@ -52,9 +54,11 @@ const loadNextOnClick=()=>{
     <div className={styles.homePage}>
       <HomeTop />
       <div className={styles.main}>
+        {(isAuth && viewedCars.length>0) &&
         <div className={styles.recentGoods}>
           <CardSlider title={"Нещодавно переглянуті товари"} cars={viewedCars} /*заменить на пересмотренные машины*/ />
         </div>
+        }
         <div className={styles.newGoods}>
           <CardSlider title={"Нові автомобілі на сайті"} cars={newCars} />
         </div>
