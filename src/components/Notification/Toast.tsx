d9d9e4dc050b579cebeca50b1e339e8a60eeff80
Martha <martasobotyk@gmail.com>
2023-11-06 14:19:22 +0200

    #add: error notification functionality for the signup page

---
+++ src/components/Notification/Toast.tsx
@@ -0,0 +1,76 @@
+import React, { SyntheticEvent, useEffect, useRef } from 'react';
+import './Toast.scss';
+import { ShowToast, HideToast } from './ShowToast';
+
+const TOAST_CONTAINER_ID = 'toast' as const;
+const TOAST_TIME_MS = 4500 as const;
+
+interface ToastProperties {
+  timer?: number;
+  label: string;
+  backgroundColor?: string;
+  color?: string;
+  borderColor?: string;
+}
+
+export const Toast: React.FC<ToastProperties> = (props) => {
+  const toast = useRef<HTMLOutputElement>(null);
+  let timer: number = -1;
+
+  const onReset = () => {
+    window.clearTimeout(timer);
+  };
+
+  const onStart = () => {
+    timer = window.setTimeout(HideToast, props.timer);
+  };
+
+  const onClose = (event: SyntheticEvent | null) => {
+    if (event) event.preventDefault();
+    HideToast();
+  };
+
+  useEffect(() => {
+    onStart();
+    toast.current?.focus();
+
+    return () => {
+      onClose(null);
+    };
+  });
+
+  return (
+    <output
+      aria-labelledby="toast-label"
+      onMouseLeave={onStart}
+      onMouseEnter={onReset}
+      className="toast"
+      ref={toast}
+      style={{
+        backgroundColor: props.backgroundColor || 'transparent',
+        color: props.color || '#555555',
+        border: props.borderColor || '#555555',
+      }}
+    >
+      <button
+        type="button"
+        onClick={onClose}
+        aria-label="Close dialog"
+        className="modal-close no-button"
+      >
+        x
+      </button>
+      <p id="toast-label">{props.label}</p>
+    </output>
+  );
+};
+
+Toast.defaultProps = {
+  timer: TOAST_TIME_MS,
+  backgroundColor: 'transparent',
+  color: '#555555',
+  borderColor: '#555555',
+};
+
+export default ShowToast;
+
--- src/pages/LoginPage/SignUpPage/SignUpPage.tsx
+++ src/pages/LoginPage/SignUpPage/SignUpPage.tsx
@@ -9,6 +9,7 @@ import googleIcon from '../../../assets/icons/google.svg';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { formReducer, initialState } from 'helpers/formReducer';
 import axios from 'axios';
+import ShowToast from '../../../components/Notification/Toast';
 
 export const SignUpPage: FC = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -25,6 +26,8 @@ export const SignUpPage: FC = () => {
   const [emailHasValue, setEmailHasValue] = useState(false);
   const [passwordHasValue, setPasswordHasValue] = useState(false);
   const [confirmPasswordHasValue, setConfirmPasswordHasValue] = useState(false);
+
+  const [messageError, setMessageError] = useState(false);
 
   const [activeField, setActiveField] = useState<FieldKey | null>(null);
 
@@ -48,98 +51,109 @@ export const SignUpPage: FC = () => {
     });
 
     switch (field) {
-    case 'name':
-      setNameHasValue(!!value);
-      break;
-    case 'email':
-      setEmailHasValue(!!value);
-      break;
-    case 'password':
-      setPasswordHasValue(!!value);
-      break;
-    case 'confirmPassword':
-      setConfirmPasswordHasValue(!!value);
-      break;
-    default:
-      break;
-  }
+      case 'name':
+        setNameHasValue(!!value);
+        break;
+      case 'email':
+        setEmailHasValue(!!value);
+        break;
+      case 'password':
+        setPasswordHasValue(!!value);
+        break;
+      case 'confirmPassword':
+        setConfirmPasswordHasValue(!!value);
+        break;
+      default:
+        break;
+    }
 
     // Перевірка на валідність та зняття помилок
     switch (field) {
       case 'name':
-         (!(value.length >= 2 && value.length <= 50))
-        ? setNameError('Ім\'я має містити від 2 до 50 символів')
-        : setNameError('');
+        !(value.length >= 2 && value.length <= 50)
+          ? setNameError("Ім'я має містити від 2 до 50 символів!")
+          : setNameError('');
         break;
 
       case 'email':
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if (!(value && emailRegex.test(value))) {
-          setEmailError('E-mail має містити @ і бути валідною адресою');
+          setEmailError('E-mail має містити @ і бути валідною адресою!');
         } else {
           setEmailError('');
         }
         break;
-      case 'password':
-        !(passwordError && value.length >= 8 && value.length <= 50 && /\d/.test(value) && /[a-zA-Z]/.test(value))
-        
-        ? setPasswordError('Пароль має містити від 8 до 50 символів, хоча б одну букву і цифру')
-        : setPasswordError('')
-        
+
+        case 'password':
+          // !(passwordError && value.length >= 8 && value.length <= 50 && /\d/.test(value) && /[a-zA-Z]/.test(value))
+          // ? setPasswordError('Пароль має містити від 8 до 50 символів, хоча б одну букву і цифру')
+          !(passwordError && value.length >= 8 && value.length <= 50 && /\d/.test(value) && /[a-z]/.test(value) && /[A-Z]/.test(value)) 
+          ? setPasswordError('Пароль має містити від 8 до 50 символів, нижнього і верхнього регістру і цифри')
+          : setPasswordError('')
         break;
+
       case 'confirmPassword':
-        !(confirmPasswordError && value === formData.password) 
-         ? setConfirmPasswordError('Паролі не співпадають')
-         : setConfirmPasswordError('')
+        !(confirmPasswordError && value === formData.password)
+          ? setConfirmPasswordError('Паролі не співпадають!')
+          : setConfirmPasswordError('');
         break;
       default:
         break;
     }
   };
 
-
   const handleBlur = (field: FieldKey) => {
     setActiveField(null);
   };
-  
+
   const handleFocus = (field: FieldKey) => {
     setActiveField(field);
   };
-  
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setNameError('');
     setPasswordError('');
     setConfirmPasswordError('');
+    setMessageError(false);
+
+    const { name, password, confirmPassword } = formData;
 
     try {
-      const URL = 'https://backend-production-7a95.up.railway.app/api/v1/authorization/register';
-      const response = await axios.post(URL, formData)
+      const URL =
+        'https://backend-production-7a95.up.railway.app/api/v1/authorization/register';
+      const response = await axios.post(URL, formData);
+
       navigate(`/login/finish-registration?email=${formData.email}`);
       dispatch({ type: 'RESET' });
-
       console.log('Успішно відправлено:', response.data);
     } catch (error) {
       console.error('Помилка відправки POST-запиту:', error);
+      setMessageError(true);
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className={styles.Login_container}>
-       <h2 className={styles.Login_title}>Реєстрація</h2>
-       <span className={styles.Login_login}>
-         Вже є акаунт?<NavLink to="/login/log-in" className={styles.Login_login_link}>Увійти</NavLink>
+      <h2 className={styles.Login_title}>Реєстрація</h2>
+      <span className={styles.Login_login}>
+        Вже є акаунт?
+        <NavLink to="/login/log-in" className={styles.Login_login_link}>
+          Увійти
+        </NavLink>
       </span>
       <div className={styles.Container}>
         <div className={styles.input_container}>
-        { nameHasValue ? (
-            <label htmlFor="name" className={
-              (activeField !== 'name' && nameError )
-                ? `${styles.Login_label_red}`
-                : `${styles.Login_label}`
-            }>
-             Введіть повне ім'я
+          {nameHasValue ? (
+            <label
+              htmlFor="name"
+              className={
+                activeField !== 'name' && nameError
+                  ? `${styles.Login_label_red}`
+                  : `${styles.Login_label}`
+              }
+            >
+              Введіть повне ім'я
             </label>
           ) : null}
           <input
@@ -158,20 +172,23 @@ export const SignUpPage: FC = () => {
             onChange={(e) => handleFieldChange('name', e.target.value)}
             required
           />
-          
+
           {activeField !== 'name' && nameError.length > 0 && (
             <span className={styles.Login_label_errorMessage}>{nameError}</span>
           )}
         </div>
 
         <div className={styles.input_container}>
-        { emailHasValue ? (
-            <label htmlFor="email" className={
-              (activeField !== 'email' && emailError )
-                ? `${styles.Login_label_red}`
-                : `${styles.Login_label}`
-            }>
-             E-mail
+          {emailHasValue ? (
+            <label
+              htmlFor="email"
+              className={
+                activeField !== 'email' && emailError
+                  ? `${styles.Login_label_red}`
+                  : `${styles.Login_label}`
+              }
+            >
+              E-mail
             </label>
           ) : null}
 
@@ -185,7 +202,6 @@ export const SignUpPage: FC = () => {
                 ? `${styles.Login_field} ${styles.Login_field_warning} ${styles.Login_field_error}`
                 : styles.Login_field
             }
-            
             value={formData.email}
             onChange={(e) => handleFieldChange('email', e.target.value)}
             onBlur={() => handleBlur('email')}
@@ -193,20 +209,27 @@ export const SignUpPage: FC = () => {
             required
           />
 
-        {activeField !== 'email' && emailError.length > 0 && <span className={styles.Login_label_errorMessage}>{emailError}</span>}
+          {activeField !== 'email' && emailError.length > 0 && (
+            <span className={styles.Login_label_errorMessage}>
+              {emailError}
+            </span>
+          )}
         </div>
 
         <div className={styles.input_container}>
-          { passwordHasValue ? (
-            <label htmlFor="password" className={
-              (activeField !== 'password' && passwordError )
-                ? `${styles.Login_label_red}`
-                : `${styles.Login_label}`
-            }>
-             Введіть пароль
+          {passwordHasValue ? (
+            <label
+              htmlFor="password"
+              className={
+                activeField !== 'password' && passwordError
+                  ? `${styles.Login_label_red}`
+                  : `${styles.Login_label}`
+              }
+            >
+              Введіть пароль
             </label>
           ) : null}
-          <div >
+          <div>
             <input
               type={showPassword ? 'text' : 'password'}
               id="password"
@@ -223,33 +246,43 @@ export const SignUpPage: FC = () => {
               required
             />
             <img
-              
-              src={showPassword ? (activeField !== 'password' && passwordError ? redEye : eye) : (activeField !== 'password' && passwordError ? redEyeClose : eyeClose)}
-              alt={showPassword ? 'hide password' : 'show password' }
+              src={
+                showPassword
+                  ? activeField !== 'password' && passwordError
+                    ? redEye
+                    : eye
+                  : activeField !== 'password' && passwordError
+                  ? redEyeClose
+                  : eyeClose
+              }
+              alt={showPassword ? 'hide password' : 'show password'}
               className={styles.input_container_icon}
               onClick={togglePasswordVisibility}
             />
           </div>
 
           {activeField !== 'password' && passwordError.length > 0 && (
-            <span className={styles.Login_label_errorMessage}>{passwordError}</span>
-
+            <span className={styles.Login_label_errorMessage}>
+              {passwordError}
+            </span>
           )}
         </div>
 
         <div className={styles.input_container}>
-
-          { confirmPasswordHasValue ? (
-            <label htmlFor="password" className={
-              ( confirmPasswordError )
-                ? `${styles.Login_label_red}`
-                : `${styles.Login_label}`
-            }>
-             Підтвердіть пароль
+          {confirmPasswordHasValue ? (
+            <label
+              htmlFor="password"
+              className={
+                confirmPasswordError
+                  ? `${styles.Login_label_red}`
+                  : `${styles.Login_label}`
+              }
+            >
+              Підтвердіть пароль
             </label>
           ) : null}
-          
-          <div >
+
+          <div>
             <input
               type={showConfirmPassword ? 'text' : 'password'}
               id="confirmPassword"
@@ -260,34 +293,61 @@ export const SignUpPage: FC = () => {
                   : styles.Login_field
               }
               value={formData.confirmPassword}
-              onChange={(e) => handleFieldChange('confirmPassword', e.target.value)}
+              onChange={(e) =>
+                handleFieldChange('confirmPassword', e.target.value)
+              }
               onBlur={() => handleBlur('confirmPassword')}
               onFocus={() => handleFocus('confirmPassword')}
               required
             />
             <img
-              src={showConfirmPassword ? (confirmPasswordError ? redEye : eye) : (confirmPasswordError ? redEyeClose : eyeClose)}
-              alt={showConfirmPassword ? 'hide password' : 'show password' }
+              src={
+                showConfirmPassword
+                  ? confirmPasswordError
+                    ? redEye
+                    : eye
+                  : confirmPasswordError
+                  ? redEyeClose
+                  : eyeClose
+              }
+              alt={showConfirmPassword ? 'hide password' : 'show password'}
               className={styles.input_container_icon}
               onClick={toggleConfirmPasswordVisibility}
             />
           </div>
 
           {confirmPasswordError.length > 0 && (
-            <span className={styles.Login_label_errorMessage}>{confirmPasswordError}</span>
+            <span className={styles.Login_label_errorMessage}>
+              {confirmPasswordError}
+            </span>
           )}
         </div>
 
         <button type="submit" className={styles.Login_btn}>
           Зареєструватися
         </button>
+
+        <>
+          {messageError &&
+            ShowToast({
+              label: 'Пошта, яку ви ввели, вже існує в нашій системі',
+              timer: 4500,
+              backgroundColor: '#f1a9a9b7',
+              color: '#ff3838',
+              borderColor: '#ff3838',
+            })}
+        </>
+
         <img src={line} alt="" className={styles.Login_line} />
         <button className={styles.Login_googleBtn}>
           Зареєструватися через Google
-          <img src={googleIcon} alt="" className={styles.Login_googleBtn_icon} />
+          <img
+            src={googleIcon}
+            alt="Google Icon"
+            className={styles.Login_googleBtn_icon}
+          />
         </button>
       </div>
     </form>
   );
 };
-
