a5ec18f4179ea6806f179a192b4b26843294cf15
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-11-16 18:46:19 +0000

    Fix main

--- shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -e ./frontend_pid.file ]; then
-    pid=$(cat ./frontend_pid.file)
-    kill $pid
-    rm ./frontend_pid.file
-    echo "Frontend stopped."
-else
-    echo "Frontend is not running or pid file not found."
-fi
--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup serve -s build > frontend_log.txt 2>&1 &
-echo $! > ./frontend_pid.file
