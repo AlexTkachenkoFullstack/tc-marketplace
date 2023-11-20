418a8b7d2f3cd46de335aa24b4a416336c3259db
Baliberdin Igor <bivfloart@gmail.com>
2023-11-20 18:07:07 +0200

    card merge

--- src/components/UserPageCard/UserPageCard.tsx
+++ src/components/UserPageCard/UserPageCard.tsx
@@ -1,3 +1,7 @@
+// THIS NEED to delete later
+
+
+
 import React from 'react'
 import styles from './UserPageCard.module.scss'
 import clockSvg from 'assets/icons/clock.svg'
@@ -5,6 +9,8 @@ import favorite from 'assets/icons/favorite.svg'
 import eyeOpen from 'assets/icons/eye-open.svg'
 import { CommonBtn } from 'components/Buttons/CommonBtn'
 import { Button } from 'components/Buttons/Button'
+import { convertDate } from 'utils/convertDate';
+
 // import { CommonBtn } from 'components/Buttons/CommonBtn'
 import add from 'assets/icons/add.svg'
 
@@ -12,40 +18,25 @@ import { ICar } from 'types/IСar'
 import { CardProperty } from 'components/CardProperty/CardProperty'
 
 type CardProps = {
-    car: {
-        fileUrl: string;
-        // title: string;
-        brand: string,
-        model: string,
-        price: number;
-        views: number;
-        phoneViews: number;
-        likes: number;
-        created: string;
-        updated: string;
-        description: string;
-    }
-}
-const car: ICar = {
-    id: 1234123,
-    brand: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, optio natus quis fugit rerum veritatis incidunt obcaecati hic tempore? Laboriosam!',
-    model: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, optio natus quis fugit rerum veritatis incidunt obcaecati hic tempore? Laboriosam!',
-    year: 1993,
-    price: 200000,
-    mileage: 1000,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, optio natus quis fugit rerum veritatis incidunt obcaecati hic tempore? Laboriosam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, optio natus quis fugit rerum veritatis incidunt obcaecati hic tempore? Laboriosam!',
-    transmission: 'astro',
-    fuelType: 'laugh',
-    engineDisplacement: 4,
-    city: 'Kyiv',
-    created: new Date().toDateString(),
-    fileUrl: 'https://cdn.pixabay.com/photo/2017/03/27/14/56/auto-2179220_640.jpg',
-    isFavorite: false
+    data: ICar,
 }
 
+// delete this temp object when logic will be ready
+const data = {
+    brand: 'brand',
+    fileUrl: 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg',
+    model: 'model',
+    price: 200000,
+    views: 20,
+    phoneViews: 10,
+    likes: 10,
+    created: new Date().toDateString(),
+    updated: new Date().toDateString(),
+    description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.Nobis expedita accusantium voluptas soluta libero sit praesentium, et cum corporis facilis!`
+}
 
 export default function UserPageCard({
-    car
+    // data
 }: CardProps) {
 
 
@@ -53,73 +44,27 @@ export default function UserPageCard({
         return `${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} $`;
     }
 
-    const mileageToString = (mileage: number) => {
-        const string = mileage.toString()
-        if (string.length < 4) return `${string} км`
-        return `${string.split('').slice(0, -3).join('')} тис. км`
-    }
-
-
-    const getDateString = (dateString: string) => {
-        const today = new Date()
-        const carDate = new Date(dateString)
-        type date = {
-            year: number,
-            month: number,
-            day: number,
-            time?: string
-        }
-        const dateNow: date = {
-            year: today.getFullYear(),
-            month: today.getMonth(),
-            day: today.getDate(),
-        }
-        const objectDate: date = {
-            year: carDate.getFullYear(),
-            month: carDate.getMonth(),
-            day: carDate.getDate(),
-            time: `${carDate.getHours()}:${(carDate.getMinutes() < 10 ? '0' : '') + carDate.getMinutes()}`
-        }
-        if (dateNow.month === objectDate.month &&
-            dateNow.day - objectDate.day < 2) {
-            return `${dateNow.day - objectDate.day ? 'вчора' : 'сьогодні'} o ${objectDate.time}`
-        }
-        return carDate.toLocaleString('uk-UA', { day: 'numeric', month: 'short', year: 'numeric' })
-
-    }
-
-
     return (
         <div className={styles.container}>
-            <img alt='car_image' src={car?.fileUrl || 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg'} className={styles.image} />
+            <img alt='car_image' src={data?.fileUrl || 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg'} className={styles.image} />
             <div className={styles.header}>
                 <div>
-                    <p className={styles.title}>{`${car?.brand} ${car?.model || 'Model'}`}</p>
-                    <p className={styles.price}>{getPriceString(car?.price)}</p>
+                    <p className={styles.title}>{`${data?.brand} ${data?.model || 'Model'}`}</p>
+                    <p className={styles.price}>{getPriceString(data?.price)}</p>
                 </div>
             </div>
             <div className={styles.description}>
                 <div className={styles.properties}>
                     {/* to do */}
                     {/* change property params when will be correct type or interface */}
-                    <CardProperty icon={eyeOpen} name={`${car.views} переглядів` || 'нема переглядів'} />
-                    {/* to do */}
-                    {/* change property params when will be correct type or interface */}
-                    <CardProperty icon={add} name={`${car.phoneViews}` || 'нема показів телефону'} />
-                    {/* to do */}
-                    {/* change property params when will be correct type or interface */}
-                    <CardProperty icon={favorite} name={car.likes.toString()} />
-                    {/* to do */}
-                    {/* change property params when will be correct type or interface */}
-                    <CardProperty icon={clockSvg} name={getDateString(car.created)} />
-                    {/* to do */}
-                    {/* change property params when will be correct type or interface 
-                    Contact with backend and consult about car update date
-                    */}
-                    <CardProperty icon={clockSvg} name={getDateString(car.updated)} />
+                    <CardProperty icon={eyeOpen} name={`${data.views} переглядів` || 'нема переглядів'} />
+                    <CardProperty icon={add} name={`${data.phoneViews}` || 'нема показів телефону'} />
+                    <CardProperty icon={favorite} name={data.likes.toString()} />
+                    <CardProperty icon={clockSvg} name={convertDate(data.created)} />
+                    <CardProperty icon={clockSvg} name={convertDate(data.updated)} />
                 </div>
                 <p className={styles.description__text}>
-                    {car?.description}
+                    {data?.description}
                 </p>
                 <div className={styles.description__buttons}>
                     <CommonBtn
--- src/pages/UserPage/MyAds/Card/Card/Card.tsx
+++ src/pages/UserPage/MyAds/Card/Card/Card.tsx
@@ -1,80 +1,76 @@
-import React, { useState } from 'react';
-import styles from './Card.module.scss';
-import eye from 'assets/icons/eye-open.svg';
-import empty from 'assets/icons/empty_icon.svg';
-import favorite from 'assets/icons/favorite.svg';
-import clock from 'assets/icons/clock.svg';
-import { ICar } from 'types/IСar';
+import styles from './UserPageCard.module.scss'
+import clockSvg from 'assets/icons/clock.svg'
+import favorite from 'assets/icons/favorite.svg'
+import eyeOpen from 'assets/icons/eye-open.svg'
+import { CommonBtn } from 'components/Buttons/CommonBtn'
 import { convertDate } from 'utils/convertDate';
+import add from 'assets/icons/add.svg'
+import { ICar } from 'types/IСar'
+import { CardProperty } from 'components/CardProperty/CardProperty'
 
-export interface CardProps {
-    data: {
-        imageUrl: string;
-        title: string;
-        price: string;
-        views: number;
-        phoneViews: number;
-        likes: number;
-        createdAt: string;
-        updatedAt: string;
-        description: string;
-    };
+type CardProps = {
+    data: ICar,
 }
 
-const Card: React.FC<CardProps> = ({ data }) => {
-    const [isButtonActive, setIsButtonActive] = useState(false);
+// delete this temp object when logic will be ready
+const data = {
+    brand: 'brand',
+    fileUrl: 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg',
+    model: 'model',
+    price: 200000,
+    views: 20,
+    phoneViews: 10,
+    likes: 10,
+    created: new Date().toDateString(),
+    updated: new Date().toDateString(),
+    description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.Nobis expedita accusantium voluptas soluta libero sit praesentium, et cum corporis facilis!`
+}
 
-    const handleButtonClick = () => {
-        setIsButtonActive(!isButtonActive);
-    };
+export default function UserPageCard({
+    // data
+}: CardProps) {
+
+
+    const getPriceString = (number: number) => {
+        return `${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} $`;
+    }
 
     return (
-        <div>
-            <div className={styles.card}>
-                <img src={data.imageUrl} alt="Car_image" className={styles.card_img} />
-                <div className={styles.card_info}>
-                    <h3 className={styles.card_info_title}>{data.title}</h3>
-                    <p className={styles.card_info_price}>{data.price}</p>
-                    <div className={styles.card_info_meta}>
-                        <div className={styles.card_info_views}>
-                            <img className={styles.icon} src={eye} alt="Eye icon" />
-                            <p className={styles.card_info_data}>{`${data.views} переглядів`}</p>
-                        </div>
-                        <div className={styles.card_info_phoneViews}>
-                            <img className={styles.icon} src={empty} alt="Empty icon" />
-                            <p className={styles.card_info_data}>{`${data.phoneViews} покази телефону`}</p>
-                        </div>
-                        <div className={styles.card_info_likes}>
-                            <img className={styles.icon} src={favorite} alt="Favorite icon" />
-                            <p className={styles.card_info_data}>{data.likes}</p>
-                        </div>
-                        <div className={styles.card_info_createdAt}>
-                            <img className={styles.icon} src={clock} alt="Clock icon" />
-                            <p className={styles.card_info_data}>{data.createdAt}</p>
-                        </div>
-                        <div className={styles.card_info_updatedAt}>
-                            <img className={styles.icon} src={clock} alt="Clock icon" />
-                            <p className={styles.card_info_data}>{data.updatedAt}</p>
-                        </div>
-                    </div>
+        <div className={styles.container}>
+            <img alt='car_image' src={data?.fileUrl || 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg'} className={styles.image} />
+            <div className={styles.header}>
+                <div>
+                    <p className={styles.title}>{`${data?.brand} ${data?.model || 'Model'}`}</p>
+                    <p className={styles.price}>{getPriceString(data?.price)}</p>
+                </div>
+            </div>
+            <div className={styles.description}>
+                <div className={styles.properties}>
+                    {/* to do */}
+                    {/* change property params when will be correct type or interface */}
+                    <CardProperty icon={eyeOpen} name={`${data.views} переглядів` || 'нема переглядів'} />
+                    <CardProperty icon={add} name={`${data.phoneViews}` || 'нема показів телефону'} />
+                    <CardProperty icon={favorite} name={data.likes.toString()} />
+                    <CardProperty icon={clockSvg} name={convertDate(data.created)} />
+                    <CardProperty icon={clockSvg} name={convertDate(data.updated)} />
+                </div>
+                <p className={styles.description__text}>
+                    {data?.description}
+                </p>
+                <div className={styles.description__buttons}>
+                    <CommonBtn
+                        children={'Редагувати'}
+                        className={styles.button_primary}
+                        onClick={() => { alert('redact') }}
+                    />
+                    <CommonBtn
+                        children={'Видалити'}
+                        className={styles.button_secondary}
+                        onClick={() => { alert('delete') }}
+                    />
 
-                    <p className={styles.card_info_description}>{data.description}</p>
-
-                    <div className={styles.card_buttons}>
-                        <button className={styles.button} onClick={handleButtonClick}>
-                            Редагувати
-                        </button>
-                        <button className={styles.button}>Видалити</button>
-                    </div>
                 </div>
             </div>
         </div>
-    );
-};
-
-export default Card;
-
-
-
-
-
+    )
+}
