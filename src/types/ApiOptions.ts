7093973295455d716784d517ace12209aafc0eaf
Eleonora Demikh <deademikh@gmail.com>
2023-11-29 19:50:22 +0200

    change hash router

--- src/types/ApiOptions.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-cycle */
-import { ProductCategory } from '.';
-
-export interface ApiOptions {
-  productCategory: ProductCategory;
-  searchParams: URLSearchParams | string;
-}
--- src/types/IRegion.ts
+++
@@ -1,4 +0,0 @@
-export interface IRegion{
-    regionId: number,
-    region: string
-}
