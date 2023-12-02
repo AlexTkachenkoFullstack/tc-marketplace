ba74655aa536c687bc53a28f771bdac4d472a7f9
Baliberdin Igor <bivfloart@gmail.com>
2023-12-02 05:33:00 +0200

    add filter wrapper

--- src/components/RangeInput/InputRange.tsx
+++ src/components/RangeInput/InputRange.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
 import InputTwoThumb from './subComponents/InputTwoThumb'
-import styles from './InputRange.module.scss'
-
-import shevron from 'assets/icons/arrow-down.svg'
-import close from 'assets/icons/close.svg'
+import FIlterWrapper from 'components/AdvSeachFilterWrapper/FIlterWrapper'
 
 type Props = {
     title: string,
@@ -18,36 +14,10 @@ type Props = {
 }
 
 export default function InputRange(props: Props) {
-
-    const [isActive, setIsActive] = useState(false)
     const { title } = props
     return (
-        <div className={styles.container}>
-            <div className={styles.title}>
-                <h3 className={styles.title_text}>{title}</h3>
-
-                <button onClick={() => {
-                    setIsActive(prev => !prev)
-                }}
-                    className={styles.iconBtn}
-                >
-                    {isActive ?
-                        <img
-                            src={shevron}
-                            alt="open menu"
-                            className={styles.icon}
-                        />
-                        : <img
-                            src={close}
-                            alt="close menu"
-                            className={styles.icon}
-                        />
-                    }
-                </button>
-            </div>
-            <div className={`${styles.content} ${isActive ? styles.content_active : ' '}`}>
-                <InputTwoThumb {...props} />
-            </div>
-        </div>
+        <FIlterWrapper title={title}>
+            <InputTwoThumb {...props} />
+        </FIlterWrapper>
     )
 }
