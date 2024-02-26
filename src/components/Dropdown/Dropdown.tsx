5f11989dbf22ec31e553962a0dd0f0e8fc73835a
dimside <dimside29@gmail.com>
2024-02-26 21:27:04 +0200

    add-styles-for-dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,8 +1,5 @@
-import {
-    FC, useState, useRef, useCallback, useEffect
-} from 'react';
+import { FC, useState, useRef, useCallback, useEffect } from 'react';
 import styles from './Dropdown.module.scss';
-import { useLocation } from 'react-router-dom';
 import arrowDown from '../../assets/icons/arrow-down.svg';
 import close from '../../assets/icons/close.svg';
 
@@ -11,195 +8,180 @@ import DropdownList from './components/DropdownList';
 import DropdownInput from './components/DropdownInput';
 
 type Props = {
-    label: string;
-    startValue: string;
-    options: string[];
-    checkboxAllowed?: boolean;
-    isDissabled?: boolean,
-    option: string | string[],
-    setOption: React.Dispatch<React.SetStateAction<string | string[]>>,
-    allOptionsLabel?: string,
-    carMark?: string | string[],
+  label: string;
+  startValue: string;
+  options: string[];
+  checkboxAllowed?: boolean;
+  isDissabled?: boolean;
+  option: string | string[];
+  setOption: React.Dispatch<React.SetStateAction<string | string[]>>;
+  allOptionsLabel?: string;
+  carMark?: string | string[];
+  updateStyle?: string;
 };
 
-export const Dropdown: FC<Props> = (props) => {
-    const {
-        label,
-        startValue,
-        options,
-        checkboxAllowed,
-        isDissabled,
-        allOptionsLabel,
-        option,
-        carMark,
-        setOption,
-    } = props;
-    const location = useLocation();
-    const isAdvancedSearchPage = location.pathname === '/advanced-search';
-    const [isActive, setIsActive] = useState(false);
-    // const [option, setOption] = useState<string | string[]>(startValue);
-    const [filterValue, setfilterValue] = useState('')
-    const [checkedValue, setCheckedValue] = useState<string[]>([]);
+export const Dropdown: FC<Props> = props => {
+  const {
+    label,
+    startValue,
+    options,
+    checkboxAllowed,
+    isDissabled,
+    allOptionsLabel,
+    option,
+    carMark,
+    setOption,
+    updateStyle,
+  } = props;
 
-    const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isActive, setIsActive] = useState(false);
+  // const [option, setOption] = useState<string | string[]>(startValue);
+  const [filterValue, setfilterValue] = useState('');
+  const [checkedValue, setCheckedValue] = useState<string[]>([]);
 
-    useEffect(()=>{
-        setCheckedValue([])
-    },[carMark])
-        
-    
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-    const setChecked = (newOption: string) => {
-        const newState = [...checkedValue, newOption]
-        setCheckedValue(newState)
-        setOption(newState)
+  useEffect(() => {
+    setCheckedValue([]);
+  }, [carMark]);
+
+  const setChecked = (newOption: string) => {
+    const newState = [...checkedValue, newOption];
+    setCheckedValue(newState);
+    setOption(newState);
+  };
+  const setUnchecked = (newOption: string) => {
+    const newState = checkedValue.filter(value => value !== newOption);
+    if (newState.length === 0) {
+      setCheckedValue([]);
+      setOption(startValue);
+      return;
     }
-    const setUnchecked = (newOption: string) => {
-        const newState = checkedValue.filter(value => value !== newOption)
-        if (newState.length === 0) {
-            setCheckedValue([])
-            setOption(startValue)
-            return
-        }
-        setCheckedValue(newState)
-        setOption(newState)
-    }
+    setCheckedValue(newState);
+    setOption(newState);
+  };
 
-    const closeDropdown = () => {
-        setIsActive(false)
-        setfilterValue('')
-    }
+  const closeDropdown = () => {
+    setIsActive(false);
+    setfilterValue('');
+  };
 
-    const changeOption = (newOption: string) => {
-        if (checkboxAllowed) {
-            if (checkedValue.includes(newOption)) {
-                setUnchecked(newOption)
-                return
-            }
-            setChecked(newOption)
-            return
-        }
-
-        setOption(newOption)
-        closeDropdown()
-    };
-
-    const renderPlaceholder = (): string => {
-
-        const length = option.length
-        if (length === 1) return option[0]
-        if (length > 1 && length < 5) return `Обрано ${length} варіанти`
-        return `Обрано ${length} варіантів`
+  const changeOption = (newOption: string) => {
+    if (checkboxAllowed) {
+      if (checkedValue.includes(newOption)) {
+        setUnchecked(newOption);
+        return;
+      }
+      setChecked(newOption);
+      return;
     }
 
-    const filterValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setfilterValue(e.target.value)
+    setOption(newOption);
+    closeDropdown();
+  };
+
+  const renderPlaceholder = (): string => {
+    const length = option.length;
+    if (length === 1) return option[0];
+    if (length > 1 && length < 5) return `Обрано ${length} варіанти`;
+    return `Обрано ${length} варіантів`;
+  };
+
+  const filterValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setfilterValue(e.target.value);
+  };
+
+  const checkboxHandler = (currentOption: string) => {
+    if (currentOption === 'none') {
+      setCheckedValue([]);
+      setOption(startValue);
+      return;
     }
-
-    const checkboxHandler = (currentOption: string) => {
-        if (currentOption === 'none') {
-            setCheckedValue([])
-            setOption(startValue)
-            return
-        }
-        if (checkedValue.includes(currentOption)) {
-            setUnchecked(currentOption)
-            return
-        }
-        setChecked(currentOption)
+    if (checkedValue.includes(currentOption)) {
+      setUnchecked(currentOption);
+      return;
     }
+    setChecked(currentOption);
+  };
 
-    useClickOutside(dropdownRef, useCallback(() => {
-        closeDropdown()
-    }, []));
+  useClickOutside(
+    dropdownRef,
+    useCallback(() => {
+      closeDropdown();
+    }, []),
+  );
 
+  useEffect(() => {
+    setfilterValue('');
+    setCheckedValue([]);
+  }, []);
 
-    useEffect(() => {
-        setfilterValue('')
-        setCheckedValue([])
-    }, [])
+  useEffect(() => {
+    setOption(startValue);
+  }, [setOption, startValue]);
 
+  return (
+    <div className={styles.container} ref={dropdownRef}>
+      {((isActive && filterValue.trim().length === 0) ||
+        option !== startValue) && <span className={styles.label}>{label}</span>}
 
-    useEffect(() => {
-        setOption(startValue);
-    }, [setOption, startValue]);
-
-    return (
+      <button
+        className={`${styles.trigger} ${isActive ? styles.trigger_active : ''}`}
+        type="button"
+        disabled={isDissabled}
+        onClick={() => {
+          setfilterValue('');
+          setIsActive(prevState => !prevState);
+        }}
+      >
         <div
-            className={styles.container}
-            ref={dropdownRef}
+          className={`${styles.trigger_content} ${
+            isActive ? styles.trigger_content_active : ''
+          } ${updateStyle && styles[updateStyle]}`}
         >
-            {((isActive && filterValue.trim().length === 0) || option !== startValue) && (
-                <span className={styles.label}>{label}</span>
+          {isActive ? (
+            <DropdownInput
+              {...props}
+              option={option}
+              filterValue={filterValue}
+              filterValueHandler={filterValueHandler}
+              placeholder={renderPlaceholder()}
+            />
+          ) : (
+            <div className={styles.text}>
+              {Array.isArray(option) ? renderPlaceholder() : option}
+            </div>
+          )}
+
+          <div
+            className={styles.icons}
+            onClick={e => {
+              if (isActive && filterValue.length > 0) {
+                e.stopPropagation();
+                setfilterValue('');
+              }
+            }}
+          >
+            {!isActive ? (
+              <img src={arrowDown} alt="down" className={styles.icon} />
+            ) : (
+              <img src={close} alt="close" className={styles.icon} />
             )}
-
-            <button
-                className={`${styles.trigger} ${isActive ? styles.trigger_active : ''}`}
-                type="button"
-                disabled={isDissabled}
-                onClick={() => {
-
-                    setfilterValue('')
-                    setIsActive((prevState) => !prevState)
-                }
-                }
-            >
-                <div className={`${styles.trigger_content} ${isActive ? styles.trigger_content_active : ''} ${isAdvancedSearchPage ? styles.advanced_search_trigger_content : ''}`}
-                >
-
-                    {isActive ? <DropdownInput
-                        {...props}
-                        option={option}
-                        filterValue={filterValue}
-                        filterValueHandler={filterValueHandler}
-                        placeholder={renderPlaceholder()}
-                    /> :
-                        <div className={styles.text}>
-                            {Array.isArray(option) ?
-                                renderPlaceholder()
-                                : option
-                            }
-                        </div>}
-
-                    <div className={styles.icons}
-                        onClick={(e) => {
-                            if (isActive && filterValue.length > 0) {
-
-                                e.stopPropagation()
-                                setfilterValue('')
-                            }
-                        }}
-                    >
-                        {!isActive ? (
-                            <img
-                                src={arrowDown}
-                                alt="down"
-                                className={styles.icon}
-                            />
-                        ) : (
-                            <img
-                                src={close}
-                                alt="close"
-                                className={styles.icon}
-
-                            />
-                        )}
-                    </div>
-                </div>
-            </button>
-
-            {isActive &&
-                <DropdownList
-                    options={options}
-                    checkboxAllowed={checkboxAllowed}
-                    filterValue={filterValue}
-                    checkedValue={checkedValue}
-                    checkboxHandler={checkboxHandler}
-                    changeOption={changeOption}
-                    allOptionsLabel={allOptionsLabel}
-                />
-            }
-
+          </div>
         </div>
-    );
+      </button>
+
+      {isActive && (
+        <DropdownList
+          options={options}
+          checkboxAllowed={checkboxAllowed}
+          filterValue={filterValue}
+          checkedValue={checkedValue}
+          checkboxHandler={checkboxHandler}
+          changeOption={changeOption}
+          allOptionsLabel={allOptionsLabel}
+        />
+      )}
+    </div>
+  );
 };
