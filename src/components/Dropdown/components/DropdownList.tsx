2913320ff98b8c5b053baf8c07740b5d94e58b73
1Kostik <sorochankostya@gmail.com>
2024-03-08 11:08:04 +0200

    up

--- src/components/Dropdown/components/DropdownList.tsx
+++ src/components/Dropdown/components/DropdownList.tsx
@@ -61,7 +61,7 @@ export default function DropdownList(props: Props) {
       const filtredItem = options?.filter(filterOptionsFunc);
       setFiltredItem(filtredItem);
     }
-  }, [options, filterOptionsFunc]);
+  }, [options,filterValue,filterOptionsFunc]);
   const filtredItems =
     optionList &&
     optionList?.map((option: any) => {
